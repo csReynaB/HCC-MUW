aa8da715fa453b333a46a91ec7212a94b1e50eb5
Carlos Reyna <creyna604@gmail.com>
2025-02-18 17:15:50 +0100

    working on progress...

--- config.py
+++
@@ -1,9 +0,0 @@
-DATA_DIR ='/home/creyna/Vogl-lab_Projects_git/HCC_MUW_analysis/Data'
-METADATA_DIR= '/home/creyna/Vogl-lab_Projects_git/HCC_MUW_analysis/Data/Metadata'
-LIB_META_DATA = 'aligent_twist_with_important_info_excludeSARS.pkl'
-PROJECT='HCC_MUW'
-GROUP_TESTS = ['Controls', 'HCC']
-
-COL_TARGET = 'group_test'
-EXTRA_FEATURES_TO_INCLUDE = ['Sex', 'Age']
-FILTERS_METADATA = {'group_test': ['Controls', 'HCC']}
